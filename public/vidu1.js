96e1ccde408473cb986511ddf96cfc56add5f595
Admin <vanpho02@gmail.com>
2016-12-26 08:31:53 +0700

    xong

--- public/vidu1.js
+++ public/vidu1.js
@@ -1,12 +1,30 @@
-class Hello extends React.Component{
+class List extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {mang: ['Android', 'iOS', 'NodeJS', 'Script']}
+  }
   render(){
     return (
-      <h1>Hello Component</h1>
+      <div>
+        <h1>Hello Component</h1>
+        {this.state.mang.map((e, i) => <Note key={i}>{e}</Note>)}
+      </div>
     );
   }
 }
 
+class Note extends React.Component{
+  render(){
+    return (
+      <div>
+        <p>{this.props.children}</p>
+        <button>Xoa</button>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <Hello/>,
+  <List/>,
   document.getElementById('root')
 );
